49370354e3779738e2933d228d4c89dbcc70bcdf
SUNGHO CHOI <wadolsh@yahoo.co.jp>
2014-04-15 06:34:05 -0400

    save

--- device/www/js/index.js
+++ device/www/js/index.js
@@ -4,4 +4,10 @@ deviceSensorApp.controller('MainCtrl', function ($scope) {
     document.addEventListener('deviceready', function() {
         $scope.ready = "ready";
     }, false);
+    
+    $scope.info = 'Device Model: '    + device.model    + '<br />' +
+                            'Device Cordova: '  + device.cordova  + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: '     + device.uuid     + '<br />' +
+                            'Device Version: '  + device.version  + '<br />';
 });
